1a298c1ce67fce82cbb5fc5ad79335472d034030
BONAM HARI VENU NARASIMHA SIDDHU <hvnsiddhu7@gmail.com>
2026-01-21 04:38:23 +0530

    Update AccountPage.js

--- frontend/src/pages/AccountPage.js
+++ frontend/src/pages/AccountPage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { API } from '../App';
 import Navbar from '../components/Navbar';
 import { Button } from '../components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../component/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../components/ui/card';
 import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
 import { Separator } from '../components/ui/separator';
